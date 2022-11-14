0291cf08c5663d086356a947d40eedb129c27018
Suresh Maskey <sureshmaskerox111@gmail.com>
2022-11-14 12:54:43 +1100

    updated with endpoints and different verbs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 8000;
 import morgan from "morgan";
 // middleware
 app.use(morgan('dev'));
+app.use(express.json())
 // api endpoints
 import taskRouter from './src/router/taskRouter.js'
 app.use("/api/v1/task",taskRouter)
--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -1,19 +1,64 @@
 import express from 'express'
 const router =express.Router()
+//fake database
+let fakeTaskTable =[
+    {   "_id":1,
+        "task":"washing",
+        "hr":44,
+        "type":"bad"
+      },
+      {"_id":2,
+        "task":"cooking",
+        "hr":44,
+        "type":"bad"
+      },
+      {"_id":3,
+        "task":"coding",
+        "hr":44,
+        "type":"bad"
+      }
+]
+
 router.post("/",(req,res)=>{
-    res.json({message:"hehe"});
+    console.log(req.body)
+    fakeTaskTable.push(req.body)
+    res.json({message:"hehe", status:"success"});
 })
 
 // R(Read)=>read data from data base and return to the client
 router.get("/",(req,res)=>{
-    res.json({message:"hehe"}); 
+
+    res.json({
+        status:"success",
+        message:"hehe",
+        data:fakeTaskTable
+}); 
 })
 //U(Update)=>update some information of existing data int the database and respond client accordingly
 router.put("/",(req,res)=>{
-    res.json({message:"update entire file"});
+    const { _id, type}= req.body
+    console.log(req.body)
+    fakeTaskTable =fakeTaskTable.map((item)=>{
+      if(item._id==_id){
+         item.type=type
+      }
+      return item
+    })
+    res.json({message:"update entire file",status:"success"});
 })
 //D(Delete)=> delete data(s)from databaseand response client accordingly
-router.delete("/",(req,res)=>{
-    res.json({message:"hehe  ia am happy huhuhu"});
+router.delete("/:_id?",(req,res)=>{
+    const {_id} = req.params;
+    // console.log(params)
+    if(!_id){
+        res.status(400).json({
+            status:"error",
+            message:"invalid request, _ID is missing"});
+            return;
+    }
+    fakeTaskTable = fakeTaskTable.filter((item)=>item._id !=_id)
+    
+    res.json({message:"update entire file",status:"success"});
+    
 })
 export default router
